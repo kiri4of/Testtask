43584940c15d9fdd8e9ce6bc78be8abc5a236673
Kiri4of <kopak7890@gmail.com>
2025-01-26 17:37:12 +0000

    fix

--- testtask/Base/Constants/AppImages.swift
+++ testtask/Base/Constants/AppImages.swift
@@ -3,7 +3,7 @@ import UIKit
 
 enum AppImages{
     static let users = UIImage(named: "users")
-    static let signUp = UIImage(named: "signUp")
+    static let signUp = UIImage(named: "signup")
     static let noInternetImage = UIImage(named: "noInternetImage")
     static let failureImage = UIImage(named: "failureImage")
     static let successImage = UIImage(named: "successImage")
